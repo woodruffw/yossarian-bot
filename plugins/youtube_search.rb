3243b6abff092d63f0a2d6e9f0c2d81250b8049b
William Woodruff <william@tuffbizz.com>
2015-04-09 21:53:12 -0400

    surround youtube queries in exception handling

--- plugins/youtube_search.rb
+++ plugins/youtube_search.rb
@@ -27,18 +27,24 @@ class YouTubeSearch < YossarianPlugin
 	match /youtube (.+)/, method: :youtube_search, strip_colors: true
 
 	def youtube_search(m, search)
-		url = URI.encode("http://gdata.youtube.com/feeds/api/videos?q=#{search}&max-results=1&v=2&prettyprint=false&alt=json")
-		hash = JSON.parse(open(url).read)
+		query = URI.encode(search)
+		url = "http://gdata.youtube.com/feeds/api/videos?q=#{query}&max-results=1&v=2&prettyprint=false&alt=json"
 
-		if hash['feed']['openSearch$totalResults']['$t'] != 0
-			entry = hash['feed']['entry'][0]
-			title = entry['title']['$t']
-			uploader = entry['author'][0]['name']['$t']
-			video_id = entry['media$group']['yt$videoid']['$t']
+		begin
+			hash = JSON.parse(open(url).read)
 
-			m.reply "#{title} [#{uploader}] - https://youtu.be/#{video_id}", true
-		else
-			m.reply "No results for #{search}.", true
+			if hash['feed']['openSearch$totalResults']['$t'] != 0
+				entry = hash['feed']['entry'][0]
+				title = entry['title']['$t']
+				uploader = entry['author'][0]['name']['$t']
+				video_id = entry['media$group']['yt$videoid']['$t']
+
+				m.reply "#{title} [#{uploader}] - https://youtu.be/#{video_id}", true
+			else
+				m.reply "No results for #{search}.", true
+			end
+		rescue Exception => e
+			m.reply e.to_s, true
 		end
 	end
 end
