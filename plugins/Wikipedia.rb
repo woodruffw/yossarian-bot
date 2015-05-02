35921b8039a03623627ad0cae6ece8ebe26dd8be
bdt0-rocklee <slackErEhth77@openmailbox.org>
2015-05-03 00:43:30 +0530

    Syntax
    Done as said in Yossarian's comments

--- plugins/Wikipedia.rb
+++ plugins/Wikipedia.rb
@@ -12,10 +12,10 @@ require 'open-uri'
 
 require_relative 'yossarian_plugin'
 
- class Wikipedia < YossarianPlugin
+class Wikipedia < YossarianPlugin
         include Cinch::Plugin
 
-        match /wiki (.+)/, method: :search_wiki
+	 match /wiki (.+)/, method: :search_wiki
 
 	def usage
 		'!wiki <search> - Search Wikipedia. '
@@ -26,15 +26,14 @@ require_relative 'yossarian_plugin'
        url = "http://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles=#{query}&format=json&exintro=1"
        hash = JSON.parse(open(url).read)
 
-		        if hash['query']['pages']['-1']
-                             m.reply "No results for #{search}."
-                    else
-                             page_id = hash['query']['pages'].keys.pop()
-                             content = hash['query']['pages'][page_id]['extract'].gsub(/([\t\r\n])|(<(\/)?b>)/, '')
-                             content = Sanitize.fragment(content).strip()
-                             content = content[0..250]
-                             m.reply "#{content}..... http://enwp.org/#{query}"
-		        end
+	if hash['query']['pages']['-1']
+               m.reply "No results for #{search}."
+        else
+               page_id = hash['query']['pages'].keys.pop()
+               content = hash['query']['pages'][page_id]['extract'].gsub(/([\t\r\n])|(<(\/)?b>)/, '')
+               content = Sanitize.fragment(content).strip()
+               m.reply "#{content} http://enwp.org/#{query}"
+	end
 
-          end
- end
+   end
+end
