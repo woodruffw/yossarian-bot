2c5d38a3cef8a32cc60b6f96db06fa34740ef0da
bdt0-rocklee <slackErEhth77@openmailbox.org>
2015-05-06 00:42:35 +0530

    Removed extra dependancies
    Used another API that uses Array instead of hashes. No need to sanitize the code or Titilize it anymore.

--- plugins/wikipedia.rb
+++ plugins/wikipedia.rb
@@ -7,8 +7,6 @@
 #  This code is licensed by slackErEhth77 under the MIT License.
 #  http://opensource.org/licenses/MIT
 
-require 'sanitize'
-require 'titleize'
 require 'json'
 require 'open-uri'
 
@@ -29,19 +27,18 @@ class Wikipedia < YossarianPlugin
 
 	def search_wiki(m, search)
 		query = URI.encode(search.titleize)
-		url = "http://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles=#{query}&format=json&exintro=1"
-		hash = JSON.parse(open(url).read)
+		url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=#{query}"
+		array = JSON.parse(open(url).read)
 
-			if hash['query']['pages']['-1']
-				m.reply "No results for #{search}."
-			else
-				page_id = hash['query']['pages'].keys.pop()
-				content = Sanitize.clean(hash['query']['pages'][page_id]['extract']).gsub(/[\t\r\n]/, '')
-				if content.empty? || content =~ /This is a redirect/
-					m.reply "http://enwp.org/#{query} - [No extract provided]", true
-				else
-					m.reply "http://enwp.org/#{query} - #{content}", true
-				end
-			end
+        if array[1].empty?
+			m.reply "Sorry no result for #{query}"
+
+		else
+
+			content = array[2].first
+		    link = array[3].first.sub('https://en.wikipedia.org/wiki/', 'http://enwp.org/')
+			m.reply "#{link} - #{content}"
+
+		end
 	end
 end
