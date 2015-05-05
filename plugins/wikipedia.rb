e24d2a4dc13becce656b50c531f4491dd1310452
William Woodruff <william@tuffbizz.com>
2015-05-05 13:24:30 -0400

    rewrite wikipedia.rb to use opensearch instead of query

--- plugins/wikipedia.rb
+++ plugins/wikipedia.rb
@@ -1,14 +1,12 @@
 #  -*- coding: utf-8 -*-
 #  wikipedia.rb
-#  Author: slackR | slackErEhth77
+#  Author: slackR (slackErEhth77) and William Woodruff
 #  ------------------------
 #  A Cinch plugin to get info from Wikipedia for yossarian-bot.
 #  ------------------------
 #  This code is licensed by slackErEhth77 under the MIT License.
 #  http://opensource.org/licenses/MIT
 
-require 'sanitize'
-require 'titleize'
 require 'json'
 require 'open-uri'
 
@@ -28,20 +26,25 @@ class Wikipedia < YossarianPlugin
 	match /wiki (.+)/, method: :search_wiki
 
 	def search_wiki(m, search)
-		query = URI.encode(search.titleize)
-		url = "http://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles=#{query}&format=json&exintro=1"
-		hash = JSON.parse(open(url).read)
+		query = URI.encode(search)
+		url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=#{query}"
 
-			if hash['query']['pages']['-1']
-				m.reply "No results for #{search}."
-			else
-				page_id = hash['query']['pages'].keys.pop()
-				content = Sanitize.clean(hash['query']['pages'][page_id]['extract']).gsub(/[\t\r\n]/, '')
-				if content.empty? || content =~ /This is a redirect/
-					m.reply "http://enwp.org/#{query} - [No extract provided]", true
+		begin
+			results = JSON.parse(open(url).read)
+			unless results[1].empty?
+				if results[2][0].empty?
+					content = "No extract provided."
 				else
-					m.reply "http://enwp.org/#{query} - #{content}", true
+					content = results[2][0]
 				end
+				link = results[3][0].sub('https://en.wikipedia.org/wiki/', 'http://enwp.org/')
+
+				m.reply "#{link} - #{content}", true
+			else
+				m.reply "No results for #{search}.", true
 			end
+		rescue Exception => e
+			m.reply e.to_s, true
+		end
 	end
 end
