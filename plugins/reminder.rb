e925aa76c89e620fd180ecd0991e50b91110f71e
bdt0-rocklee <slackErEhth77@openmailbox.org>
2015-06-22 22:49:20 +0530

    reminder.rb

---
+++ plugins/reminder.rb
@@ -0,0 +1,35 @@
+#  -*- coding: utf-8 -*-
+#  wikipedia.rb
+#  Author: slackR | slackErEhth77
+#  ------------------------
+#  A Cinch plugin to get info from Wikipedia for yossarian-bot.
+#  ------------------------
+#  This code is licensed by slackErEhth77 under the MIT License.
+#  http://opensource.org/licenses/MIT
+
+require_relative 'yossarian_plugin'
+
+	class Reminder < YossarianPlugin
+		include Cinch::Plugin
+		
+		def usage
+				"!remind <count> <unit> 'Message'."
+		end
+		
+		def match(cmd)
+				cmd =~ /^(!)?remind$/
+		end
+	
+	match /remind (\d+) (\w+) (.+)/, method: :set_reminder
+		
+		def convert_to_sec(count, unit)
+				count = (count*60 ) if unit =~ /min/
+				count = (count*3600) if unit =~ /hour/
+				return count
+		end
+		
+		def set_reminder(m, count, unit, message)
+				  count = convert_to_sec(count, unit) if unit =~ /min|hour/
+				  Timer(count) { m.reply "#{message}" }
+		end
+	end
