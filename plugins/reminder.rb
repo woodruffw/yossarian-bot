702ad0c82c7be7d7561a224f57680f8f06b3a7bc
bdt0-rocklee <slackErEhth77@openmailbox.org>
2015-06-22 23:17:25 +0530

    Update reminder.rb

--- plugins/reminder.rb
+++ plugins/reminder.rb
@@ -7,18 +7,18 @@
 #  This code is licensed by slackErEhth77 under the MIT License.
 #  http://opensource.org/licenses/MIT
 
- require_relative 'yossarian_plugin' 
+require_relative 'yossarian_plugin' 
  
 class Reminder	< YossarianPlugin
 		include Cinch::Plugin
 		use_blacklist
 		
 		def usage
-			"!remind <count> <unit> 'Message'."
+	       "!remind <count> <unit> 'Message'."
 		end
 		
 		def match(cmd)
-			cmd =~ /^(!)?remind$/
+		  cmd =~ /^(!)?remind$/
 		end
 	
 	match /remind (\d+) (\w+) (.+)/, method: :set_reminder
@@ -30,11 +30,11 @@ class Reminder	< YossarianPlugin
 	    else
 	      count = (count*3600)
 	    end				
-		return count
+	 return count
 	end
 		
 	def set_reminder(m, count, unit, message)
-		count = convert_to_sec(count, unit) if unit =~ /min|hour/
-		Timer(count) { m.reply "#{message}" }
+    	count = convert_to_sec(count, unit) if unit =~ /min|hour/
+	    Timer(count) { m.reply "#{message}" }
 	end
 end
