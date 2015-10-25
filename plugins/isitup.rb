dd7d47726058ea6202fa0bd083ea7e5da9084ada
William Woodruff <william@tuffbizz.com>
2015-10-25 15:27:37 -0400

    update to https

--- plugins/isitup.rb
+++ plugins/isitup.rb
@@ -16,7 +16,7 @@ class IsItUp < YossarianPlugin
 	include Cinch::Plugin
 	use_blacklist
 
-	URL = 'http://isitup.org/%{domain}.json'
+	URL = 'https://isitup.org/%{domain}.json'
 
 	def usage
 		'!isitup <site> - Check whether or not <site> is currently online. Alias: !up.'
