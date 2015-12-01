a4c49db76bed7801bebd8e8d172192211d1a7d5e
William Woodruff <william@tuffbizz.com>
2015-11-30 21:39:27 -0500

    update Beedogs to use beedog.github.io, bump version

--- plugins/beedogs.rb
+++ plugins/beedogs.rb
@@ -8,14 +8,22 @@
 #  This code is licensed by William Woodruff under the MIT License.
 #  http://opensource.org/licenses/MIT
 
+require 'open-uri'
+
 require_relative 'yossarian_plugin'
 
 class Beedogs < YossarianPlugin
 	include Cinch::Plugin
 	use_blacklist
 
-	COUNT = 327
-	URL = 'http://beedogs.com/index_files/image%{num}.jpg'
+	URL = 'http://beedog.github.io'
+	COUNT_URL = "#{URL}/count"
+	IMAGE_URL = "#{URL}/image%{num}.png"
+
+	def initialize(*args)
+		super
+		@count = open(COUNT_URL).read.to_i
+	end
 
 	def usage
 		'!beedog - Retrieve a random picture of a beedog.'
@@ -28,8 +36,8 @@ class Beedogs < YossarianPlugin
 	match /beedog$/, method: :beedog
 
 	def beedog(m)
-		num = '%03d' % rand(1..COUNT)
-		url = URL % { num: num }
+		num = rand(1..@count)
+		url = IMAGE_URL % { num: num }
 
 		m.reply url, true
 	end
