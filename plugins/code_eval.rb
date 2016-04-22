fa001d88858a3a2ea8a4e5c9462b92b3682bc4be
William Woodruff <william@tuffbizz.com>
2016-04-21 23:52:57 -0400

    CodeEval: Replace web scraping in plugin with ruby-eval-in.
    ruby-eval-in encapsulates eval.in in a ruby gem, hiding the web scraping
    aspect from the bot.
    
    Bump version, update Gemfile(.lock).
    
    Closes #39.

--- plugins/code_eval.rb
+++ plugins/code_eval.rb
@@ -8,35 +8,13 @@
 #  This code is licensed by William Woodruff under the MIT License.
 #  http://opensource.org/licenses/MIT
 
-require 'mechanize'
-require 'nokogiri'
+require 'eval-in'
 
 require_relative 'yossarian_plugin'
 
 class CodeEval < YossarianPlugin
 	include Cinch::Plugin
 	use_blacklist
-
-	URL = 'http://eval.in'
-	LANGS = {
-		'c' => 'c/gcc-4.9.1',
-		'c++' => 'c++/gcc-4.9.1',
-		'coffeescript' => 'coffeescript/node-0.10.29-coffee-1.7.1',
-		'fortran' => 'fortran/f95-4.4.3',
-		'haskell' => 'haskell/hugs98-sep-2006',
-		'io' => 'io/io-20131204',
-		'js' => 'javascript/node-0.10.29',
-		'lua' => 'lua/lua-5.2.3',
-		'ocaml' => 'ocaml/ocaml-4.01.0',
-		'php' => 'php/php-5.5.14',
-		'pascal' => 'pascal/fpc-2.6.4',
-		'perl' => 'perl/perl-5.20.0',
-		'python' => 'python/cpython-3.4.1',
-		'python2' => 'python/cpython-2.7.8',
-		'ruby' => 'ruby/mri-2.2',
-		'slash' => 'slash/slash-head',
-		'nasm' => 'assembly/nasm-2.07'
-	}
 
 	def usage
 		'!eval <lang> <code> - Evaluate the given code with the given language on eval.in.'
@@ -49,37 +27,14 @@ class CodeEval < YossarianPlugin
 	match /eval (\S+) (.+)/, method: :code_eval, strip_colors: true
 
 	def code_eval(m, lang, code)
-		lang.downcase!
+		begin
+			result = EvalIn.eval(lang, code)
 
-		if LANGS.key?(lang)
-			mech = Mechanize.new
-			mech.user_agent_alias = 'Linux Firefox'
-			page = mech.get(URL)
-
-			begin
-				form = page.forms.first
-				form.field_with(:name => 'code').value = code
-				form.field_with(:name => 'lang').value = LANGS[lang]
-				html = Nokogiri::HTML(mech.submit(form).body)
-				results = html.css('pre').last.text.normalize_whitespace
-
-				if results.size > 0
-					m.reply Sanitize(results), true
-				else
-					error = html.css('p')[1].text.strip
-
-					# 'Error: OK' is a terrible error message
-					if error !~ /^OK/
-						m.reply "Error: #{error}.", true
-					else
-						m.reply 'Error: No output produced.', true
-					end
-				end
-			rescue Exception => e
-				m.reply e.to_s, true
-			end
-		else
+			m.reply result.output.normalize_whitespace, true
+		rescue EvalIn::BadLanguageError
 			m.reply "I don\'t know #{lang}.", true
+		rescue EvalIn::ConnectionError
+			m.reply "Failure while connecting to evaluation service.", true
 		end
 	end
 end
