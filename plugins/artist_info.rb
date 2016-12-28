db506ec75b945800ae418c0f59b07646584afac4
William Woodruff <william@tuffbizz.com>
2016-12-27 22:27:33 -0500

    artist_info: Fix variable typo.

--- plugins/artist_info.rb
+++ plugins/artist_info.rb
@@ -41,7 +41,7 @@ class ArtistInfo < YossarianPlugin
 			begin
 				info = @lastfm.artist.get_info(artist: artist)
 
-				if indo && !info['mbid'].empty?
+				if info && !info['mbid'].empty?
 					info.default = '?'
 					name = info['name']
 					url = info['url']
