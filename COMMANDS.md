924d58ec6095915945f3226810c559fa8dcb5e55
William Woodruff <william@tuffbizz.com>
2017-03-02 14:42:23 -0500

    New plugin: FieriQuotes (!fieri)
    Gets a random Guy Fieri quote, and lowers the self-respect of the bot.
    
    Bump version, update plugins and COMMANDS to reflect new plugin.

--- COMMANDS.md
+++ COMMANDS.md
@@ -36,6 +36,7 @@ Command Syntax | Plugin class | Description | Working?
 `!define <word>` | MerriamWebster | Get the [Merriam-Webster](http://www.merriam-webster.com/) definiton of a word. | Yes
 `!dinner` | Dinner | Retrieve a random dinner recipe from [whatthefuckshouldimakefordinner.com](http://whatthefuckshouldimakefordinner.com). | Yes
 `!eval <lang> <code>` | CodeEval | Evaluate some code on [eval.in](https://eval.in). | Yes
+`!fieri` | FieriQuotes | Get an random Guy Fieri quote. | Yes
 `!flip <down|up> <text>` | FlipText | Flip text upside down or rightside up. | Yes
 `!fortune` | Fortune | Get a Unix fortune. | Yes
 `!genre` | Genres | Generate a random music genre. | Yes
---
+++ plugins/fieri_quotes/fieri_quotes.rb
@@ -0,0 +1,33 @@
+#  -*- coding: utf-8 -*-
+#  fieri_quotes.rb
+#  Author: William Woodruff
+#  ------------------------
+#  A Cinch plugin that provides Guy Fieri quotes for yossarian-bot.
+#  fieri_quotes.txt compiled from various sources.
+#  ------------------------
+#  This code is licensed by William Woodruff under the MIT License.
+#  http://opensource.org/licenses/MIT
+
+require_relative '../yossarian_plugin'
+
+class FieriQuotes < YossarianPlugin
+  include Cinch::Plugin
+  use_blacklist
+
+  QUOTES_FILE = File.expand_path(File.join(File.dirname(__FILE__), 'fieri_quotes.txt'))
+  QUOTES = File.readlines(QUOTES_FILE)
+
+  def usage
+    '!fieri - Fetch a random Guy Fieri quote.'
+  end
+
+  def match?(cmd)
+    cmd =~ /^(!)?fieri$/
+  end
+
+  match /fieri$/, method: :theo_quote
+
+  def theo_quote(m)
+    m.reply QUOTES.sample
+  end
+end
