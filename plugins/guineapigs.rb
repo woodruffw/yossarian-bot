4ee2ca6046b5fd097ce689e8782f4d467c50b555
William Woodruff <william@tuffbizz.com>
2017-09-15 23:32:06 -0400

    GuineaPigs: Use HTTPS
    Imgur has HTTP -> HTTPS redirection enabled, so this
    is now necessary.

--- plugins/guineapigs.rb
+++ plugins/guineapigs.rb
@@ -7,17 +7,17 @@
 #  This code is licensed by Bryan Hernandez under the MIT License.
 #  http://opensource.org/licenses/MIT
 
-require 'open-uri'
-require 'nokogiri'
+require "open-uri"
+require "nokogiri"
 
-require_relative './yossarian_plugin'
+require_relative "./yossarian_plugin"
 
 class GuineaPigs < YossarianPlugin
   include Cinch::Plugin
   use_blacklist
 
   def usage
-    '!guinea - Get a random guinea pig picture.'
+    "!guinea - Get a random guinea pig picture."
   end
 
   def match?(cmd)
@@ -27,8 +27,8 @@ class GuineaPigs < YossarianPlugin
   match /guinea$/, method: :guinea
 
   def guinea(m)
-    html = Nokogiri::HTML(open("http://imgur.com/r/guineapigs/").read)
-    img = html.css("a[class=image-list-link]")[rand(1..60)]['href']
-    m.reply "http://imgur.com#{img}", true
+    html = Nokogiri::HTML(open("https://imgur.com/r/guineapigs/").read)
+    img = html.css("a[class=image-list-link]")[rand(1..60)]["href"]
+    m.reply "https://imgur.com#{img}", true
   end
 end
