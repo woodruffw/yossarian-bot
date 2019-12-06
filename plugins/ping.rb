1435732a4c5c8c000087eecc00d70f991fcdef3a
Ckat <Ckat@teknik.io>
2019-12-06 02:00:12 +0100

    add pingme plugin (#88)
    * add pingme plugin
    
    * merged pingme with ping
    
    - user a threadlocal for sent status
    - merge formats of pingme and ping
    
    * reformat output and function
    
    - dont explicitly 'return'
    - remove ternaries

--- plugins/ping.rb
+++ plugins/ping.rb
@@ -15,6 +15,15 @@ class Ping < YossarianPlugin
   include Cinch::Plugin
   use_blacklist
 
+  def initialize(*args)
+    super
+    @nick = ""
+    @channel = ""
+    @timestamp = {}
+    @thread = Thread.current
+    @thread[:ping_sent] = false
+  end
+
   def usage
     "!ping - Ping the bot for a timestamped response."
   end
@@ -23,9 +32,37 @@ class Ping < YossarianPlugin
     cmd =~ /^(!)?ping$/
   end
 
-  match /ping$/, method: :ping
+  def format_ping_time
+    milis = (Time.now - @timestamp) * 1000.0
+    mins = (milis / 60000).to_i
+    secs = (milis / 1000).to_i
+    str = ""
+    if mins > 0
+      str = "#{str}#{mins} minute(s), "
+    end
+    if secs > 0
+      str = "#{str}#{secs} second(s), "
+    end
+    "#{str}#{((milis%60000)%1000).to_i} millisecond(s)"
+  end
 
-  def ping(m)
-    m.reply "pong #{Time.now.to_i}", true
+  match /ping$/, method: :ctcp_ping_req
+  listen_to :ctcp, method: :ctcp_ping_recv
+
+  def ctcp_ping_req(m)
+    @nick = m.user.nick
+    @channel = m.channel
+    @timestamp = Time.now
+    User(@nick).ctcp "PING #{@timestamp.to_i}"
+    @thread[:ping_sent] = true
+  end
+
+  def ctcp_ping_recv(m)
+    if m.ctcp_message.include?("PING")
+      if @thread[:ping_sent]
+        Channel(@channel).send "#{@nick}: pong #{@timestamp.to_i} (#{format_ping_time})"
+        @thread[:ping_sent] = false
+      end
+    end
   end
 end
