b04dd2de9b5c856f601b19986d8bb10348ea7369
strong-code <colin@strongco.de>
2020-03-11 22:04:47 -0700

    crypto: migrate module to new CMC api

--- plugins/crypto.rb
+++ plugins/crypto.rb
@@ -9,7 +9,7 @@ class Crypto < YossarianPlugin
   include Cinch::Plugin
   use_blacklist
 
-  BASE_URL = "https://api.coinmarketcap.com/v1/ticker/"
+  BASE_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
 
   def usage
     "!crypto <coin> [currency] - Get the current price of a coin (converted to an optional currency)"
@@ -22,31 +22,26 @@ class Crypto < YossarianPlugin
   match /crypto (.+)/, method: :crypto, strip_colors: true
 
   def crypto(m, payload)
-    coin_name, currency = payload.split(" ")
-    api_endpoint        = build_url(currency)
-    res                 = JSON.parse(open(api_endpoint).read)
+    coin_name, currency = payload.split(" ").map(&:upcase)
+    currency ||= "USD"
+    api_endpoint = "#{BASE_URL}?symbol=#{coin_name}&convert=#{currency}"
 
-    coin = parse_coin_info(res, coin_name, currency)
+    response = JSON.parse(open(
+        api_endpoint,
+        "X-CMC_PRO_API_KEY" => ENV['COINMARKETCAP_API_KEY']
+      ).read)
+
+    coin = parse_coin_info(response, coin_name, currency)
 
     m.reply "1 #{coin[:sym]} (#{coin[:name]}) = #{coin[:price].to_f.round(3)} #{coin[:currency].upcase} | #{fmt_changes(coin[:changes])}", true
 
   rescue OpenURI::HTTPError => e
-    handle_error(m, e.io.status)
+    handle_error(m, e.io)
   rescue Exception => e
     m.reply e.to_s, true
   end
 
   private
-
-  def find_matching_coin(res, coin_name)
-    res.each do |c|
-      if c["name"].downcase == coin_name || c["symbol"] == coin_name.upcase
-        return c
-      end
-    end
-
-    raise "No matching coin found for #{coin_name}"
-  end
 
   def fmt_changes(changes)
     changes.map do |k,v|
@@ -55,42 +50,28 @@ class Crypto < YossarianPlugin
     end.join(" | ")
   end
 
-  def parse_coin_info(res, coin_name, currency)
-    hash = find_matching_coin(res, coin_name)
-
-    # If we cannot find the requested currency, default to usd
-    currency = "usd" if currency.nil? || hash["price_#{currency.downcase}"].nil?
+  def parse_coin_info(response, coin_name, currency)
+    hash = response["data"][coin_name]
 
     {
       name: hash["name"],
       sym: hash["symbol"],
-      price: hash["price_#{currency}"],
+      price: hash["quote"][currency]["price"],
       changes: {
-        hourly: hash["percent_change_1h"],
-        daily: hash["percent_change_24h"],
-        weekly: hash["percent_change_7d"],
+        hourly: hash["quote"][currency]["percent_change_1h"],
+        daily: hash["quote"][currency]["percent_change_24h"],
+        weekly: hash["quote"][currency]["percent_change_7d"],
       },
       currency: currency,
     }
   end
 
-  def build_url(currency)
-    url = BASE_URL
-
-    url = url + "/?convert=" + currency if currency
-
-    url
-  end
-
   def handle_error(m, status)
-    code = status[0].to_i
-    err  = status[1]
+    hash = JSON.parse(status.string)["status"]
+    code = hash["error_code"].to_i
+    err  = hash["error_message"]
 
-    if code == 404
-      m.reply "Could not find that coin", true
-    else
-      m.reply "[#{code}] #{err}", true
-    end
+    m.reply "[#{code}] #{err}", true
   end
 
 end
