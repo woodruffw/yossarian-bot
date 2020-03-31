f9e8bb99a64f1d363d020bd0bcb2e5e7404fe394
Ckat <Ckat@teknik.io>
2020-03-31 23:41:00 +0000

    add corona plugin (#98)
    * initial corona plugin
    
    - info per country only
    - using questionable worldofmeters scraper api
    
    * remove no longer needed require
    
    * add missing require
    
    this doesnt seem to be needed, but I assume thats because other plugins
    include it, should probably formally require it like this since it uses
    open-uri

---
+++ plugins/corona.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "open-uri"
+require "json"
+
+require_relative "yossarian_plugin"
+
+class Corona < YossarianPlugin
+  include Cinch::Plugin
+  use_blacklist
+
+  BASE_URL = "https://corona.lmao.ninja/countries"
+
+  def usage
+    "!corona [country] Get the current info about corona"
+  end
+
+  def match?(cmd)
+    cmd =~ /^(!)?corona/
+  end
+
+  match /corona (.+)/, method: :corona, strip_colors: true
+
+  def corona(m, country)
+    api_endpoint = "#{BASE_URL}/#{country}"
+    hash = JSON.parse(URI.open(api_endpoint).read)
+    m.reply "COVID-19 in #{hash["country"]} | Total: #{hash["cases"]} cases(#{hash["critical"]} critical), #{hash["deaths"]} deaths, #{hash["recovered"]} recovered. Today: #{hash["todayCases"]} cases, #{hash["todayDeaths"]} deaths.", true
+
+  rescue OpenURI::HTTPError => e
+    m.reply "Nothing found for country '#{country}'.", true
+  rescue Exception => e
+    m.reply e.to_s, true
+  end
+
+end
