f714b7a98ecc68532a9690a4787a4680796ed186
William Woodruff <william@yossarian.net>
2023-07-17 00:19:39 -0400

    NowPlaying: try sending as str

--- plugins/now_playing/now_playing.rb
+++ plugins/now_playing/now_playing.rb
@@ -74,7 +74,7 @@ class NowPlaying < YossarianPlugin
 
       if username
         begin
-          info = @lastfm.user.get_recent_tracks(username, limit: 1, to: Time.now.utc.to_i)
+          info = @lastfm.user.get_recent_tracks(username, limit: 1, to: Time.now.utc.to_i.to_s)
 
           if info
             # APIs should always return a uniform type...
